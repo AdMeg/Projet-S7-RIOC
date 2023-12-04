3d774c1b988c5bfe139e0ec1ab99bf4e78fbfb9d
Renan Gohaut <76920456+Hacteon@users.noreply.github.com>
2023-12-04 11:07:47 +0100

    auth api request

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,25 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'App',
-}
+  data() {
+    return {
+      answer: {},
+    };
+  },
+  methods: {
+    async getAnswer() {
+      const { data } = await axios.get("https://yesno.wtf/api");
+      this.answer = data;
+    },
+  },
+  beforeMount() {
+    this.getAnswer();
+  },
+};
 </script>
 
 
